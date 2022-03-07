3c6f43a637fbe4e2a13f7da061e0f5521342dc5c
victorraul <victor-raul-22@hotmail.com>
2022-03-07 00:48:20 -0500

    test husky

---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ",jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@container": path.resolve(__dirname, "src/containers"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      // Babel
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      // html
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      // css
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        exclude: /node_modules/,
+      },
+      // assets
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
